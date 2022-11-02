b73fa34d3ebfce2757acd36b90a32b1b56f33ea1
Feuerwerko <94636749+Feuerwerko@users.noreply.github.com>
2022-11-02 18:18:09 +0100

    Added length selection and cleaned up code.

--- random_password.py
+++ random_password.py
@@ -1,9 +1,22 @@
-import random
+from random import sample
+
 upper="ZXCVBNMLKJHGFDSAQWERTYUIOP"
-lower="zxcvbnmlkjhgfdsaqwertyuiop"
+lower=upper.lower()
 number="0123456789"
-symbol="!@#$%^&*()"
-all= upper+lower+symbol+number
-length=16
-password="".join(random.sample(all,length))
-print("Take your password:" + password)
+symbol="!@#$%^&*()/"
+all = upper+lower+symbol+number
+
+length = 16
+
+try:
+    length = input("How long do you want your generated password to be?: ")
+except Exception:
+    print("Invalid answer! try again.")
+    try:
+        length = input("How long do you want your generated password to be?: ")
+    except Exception:
+        print("Invalid answer! Using 16 letter length.")
+
+password="".join(sample(all,length))
+
+print("Generated password: " + password)
