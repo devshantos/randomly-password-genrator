c6dd525c700a80685c5a1d2f74f38b2642f83a40
Saikatul Islam <75540959+devshantos@users.noreply.github.com>
2022-11-03 10:52:34 +0600

    Update random_password.py

--- random_password.py
+++ random_password.py
@@ -1,22 +1,16 @@
 from random import sample
 
+#Write your symbol and number
 upper="ZXCVBNMLKJHGFDSAQWERTYUIOP"
 lower=upper.lower()
 number="0123456789"
 symbol="!@#$%^&*()/"
+
+#All sybol addition
 all = upper+lower+symbol+number
 
-length = 16
-
-try:
-    length = input("How long do you want your generated password to be?: ")
-except Exception:
-    print("Invalid answer! try again.")
-    try:
-        length = input("How long do you want your generated password to be?: ")
-    except Exception:
-        print("Invalid answer! Using 16 letter length.")
-
+#Password length
+length = int(input("How long you want to generated your password?: "))
 password="".join(sample(all,length))
-
 print("Generated password: " + password)
+
